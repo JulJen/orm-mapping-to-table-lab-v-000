d96c24ac70017b009f729347b036242d0d104056
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 23:25:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,8 @@
+# Each individual student will have two attributes, a name and a grade
+
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
